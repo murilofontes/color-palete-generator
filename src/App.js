fb7aa3e6e87760960411f0d5089bfa580b121b29
murilofontes <fontemurilo@gmail.com>
2021-04-14 00:41:51 -0300

    Inicio do projeto - criação dos arquivos base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ColorsList from './components/ColorsList'
+import Form from './components/Form'
+function App ()
+{
+  return (
+    <div>
+      <ColorsList />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Escreva a cor" />
+        <button>OK</button>
+      </form>
+    )
+  }
+}
+
+export default Form;
